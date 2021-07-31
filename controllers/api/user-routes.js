a53aa9fc394882ef47e73e2a3d6da2631c5007d3
Anthony Gress <the.techx.guy@gmail.com>
2021-07-30 22:26:23 -0400

    post new user to db working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,3 +2,39 @@ const router = require("express").Router();
 const { User } = require("../../models");
 
 module.exports = router;
+
+router.get("/", async (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", async (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
