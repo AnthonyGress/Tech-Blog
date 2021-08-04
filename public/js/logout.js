d63f5efcca3c2af71726a784c0ffe8360cbd86ce
Anthony Gress <the.techx.guy@gmail.com>
2021-08-04 02:00:49 -0400

    finalizing pwa

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,13 +1,16 @@
 const logout = async () => {
-  const response = await fetch("/api/user/logout", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-  });
-
-  if (response.ok) {
-    document.location.replace("/");
-  } else if (err) {
-    console.log(err);
+  try {
+    const response = await fetch("/api/user/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      console.log(response);
+    }
+  } catch {
+    console.log("fail");
     alert("Failed to log out.");
   }
 };
