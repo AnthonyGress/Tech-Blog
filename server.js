c504a53fc96879c95bd9d4c65750ba2823b5598e
Anthony Gress <the.techx.guy@gmail.com>
2021-08-04 01:19:32 -0400

    made cookie use https

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: "Super secret secret",
-  cookie: {},
+  cookie: { secure: true },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
